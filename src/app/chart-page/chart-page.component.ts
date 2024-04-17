78b787124dac2f026d517b8cbacfa7b71185d9e6
kirill kolomin <kirillvintorez@gmail.com>
2024-04-17 12:50:49 +0500

    Add adding and removing functionality

--- src/app/chart-page/chart-page.component.ts
+++ src/app/chart-page/chart-page.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ChartComponent} from "./chart/chart.component";
 import {ListOfValuesComponent} from "./list-of-values/list-of-values.component";
+import {Value} from "./domain/value";
+
+const LOCAL_STORAGE_VALUE_KEY = 'values';
 
 @Component({
   selector: 'app-chart-page',
@@ -12,6 +15,35 @@ import {ListOfValuesComponent} from "./list-of-values/list-of-values.component";
   templateUrl: './chart-page.component.html',
   styleUrl: './chart-page.component.css'
 })
-export class ChartPageComponent {
+export class ChartPageComponent implements OnInit {
+  values: Value[] = [];
 
+  ngOnInit(): void {
+    this.restoreValuesFromLocalStorage();
+  }
+
+  addNewValue(value: number): void {
+    this.values.push({
+      date: new Date().toISOString().slice(14, 23),
+      value,
+    });
+    this.setValuesToLocalStorage();
+  }
+
+  removeValue(index: number): void {
+    this.values.splice(index, 1);
+    this.setValuesToLocalStorage();
+  }
+
+  setValuesToLocalStorage(): void {
+    localStorage.setItem(LOCAL_STORAGE_VALUE_KEY, JSON.stringify(this.values));
+  }
+
+  restoreValuesFromLocalStorage(): void {
+    const storedValues = localStorage.getItem(LOCAL_STORAGE_VALUE_KEY);
+
+    if (storedValues) {
+      this.values = JSON.parse(storedValues);
+    }
+  }
 }
--- src/app/chart-page/list-of-values/list-of-values.component.ts
+++ src/app/chart-page/list-of-values/list-of-values.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+import {Value} from "../domain/value";
 
 @Component({
   selector: 'app-list-of-values',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgForOf
+  ],
   templateUrl: './list-of-values.component.html',
   styleUrl: './list-of-values.component.css'
 })
 export class ListOfValuesComponent {
+  @Input() values: Value[] = []
 
+  control = new FormControl<number>(0)
+
+  @Output() onNewValueAdd = new EventEmitter<number>();
+  @Output() onValueRemove = new EventEmitter<number>();
+
+  addValueItem(event: Event) {
+    event.stopPropagation();
+
+    this.onNewValueAdd.emit(this.control.value ?? 0);
+    this.control.setValue(0);
+  }
+
+  removeValueItem(index: number): void {
+    this.onValueRemove.emit(index);
+  }
 }
