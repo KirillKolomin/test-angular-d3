e565d188aaf1b1b5e6f6f69eaed764bed23ca945
kirill kolomin <kirillvintorez@gmail.com>
2024-04-17 12:09:58 +0500

    Add skeleton to the page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ChartPageComponent} from "./chart-page/chart-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [
+    ChartPageComponent
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- src/app/chart-page/chart-page.component.ts
+++ src/app/chart-page/chart-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ChartComponent} from "./chart/chart.component";
+import {ListOfValuesComponent} from "./list-of-values/list-of-values.component";
 
 @Component({
   selector: 'app-chart-page',
   standalone: true,
-  imports: [],
+  imports: [
+    ChartComponent,
+    ListOfValuesComponent
+  ],
   templateUrl: './chart-page.component.html',
   styleUrl: './chart-page.component.css'
 })
