bbbc4fb28f29717b10512cc8126edc476f39ac5f
kirill kolomin <kirillvintorez@gmail.com>
2024-04-17 11:45:06 +0500

    Remove not necessary things

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,23 +7,4 @@ describe('AppComponent', () => {
       imports: [AppComponent],
     }).compileComponents();
   });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'test-d3-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-d3-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-d3-angular');
-  });
 });
