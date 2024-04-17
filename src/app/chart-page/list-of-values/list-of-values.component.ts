0a02ec4601fd7a77219876107162ab44f877d516
kirill kolomin <kirillvintorez@gmail.com>
2024-04-17 11:51:45 +0500

    Add chart and list of values components

---
+++ src/app/chart-page/list-of-values/list-of-values.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-values',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-of-values.component.html',
+  styleUrl: './list-of-values.component.css'
+})
+export class ListOfValuesComponent {
+
+}
