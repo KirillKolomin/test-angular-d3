d6cdabb7ec0ceebbdd519b1d7fabf3d26fd3e988
kirill kolomin <kirillvintorez@gmail.com>
2024-04-17 11:49:46 +0500

    Add chart page component

---
+++ src/app/chart-page/chart-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-page.component.html',
+  styleUrl: './chart-page.component.css'
+})
+export class ChartPageComponent {
+
+}
